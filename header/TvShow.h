d2812b7d75185182fc27e64ed00ff96686837ed4
celinavasquez <cvasq043@ucr.edu>
2025-02-23 19:39:56 -0800

    Created TvShow class and files

---
+++ header/TvShow.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class TVShow {
+private:
+    std::string title;
+    std::string genre;
+    std::string director;
+    std::string rating;
+public:
+    const void setPreferences(std::string title, std::string genre, std::string director, std::string rating) const;
+};
