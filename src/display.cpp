0422d87c65d98b01a4c8ffc78b4cc2b6b475976d
celinavasquez <cvasq043@ucr.edu>
2025-02-26 19:49:53 -0800

    Finished developer's recommendation function

--- src/display.cpp
+++ src/display.cpp
@@ -1,4 +1,4 @@
-#include "display.h"
+#include "../header/display.h"
 #include <iostream>
 
 
@@ -7,7 +7,10 @@ void Display::Menu() {
 }
 
 void Display::displayRecommendationsDev() {
-
+    std::cout << "Motunrayo's Recommendations: Abbott Elementary, Criminal Minds, The Next Step\n";
+    std::cout << "Celina's Recommendations: The Office, Brooklyn Nine-Nine, Bridgerton\n";
+    std::cout << "Megan's Recommendations: What We Do in the Shadows, The Good Place, Killing Eve\n";
+    std::cout << "Nam's Recommendations: House of Cards, Euphoria, Sex and the City\n";
 }
 
 void Display::displayRecommendationsRand() {
