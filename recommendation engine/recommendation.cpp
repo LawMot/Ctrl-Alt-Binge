ca294d0bb6122e56458a07a5239d9268a727cf64
LawMot <molawr12@gmail.com>
2025-02-26 21:10:57 -0800

    user enter preferences (also updated recommendation.cpp)

--- recommendation engine/recommendation.cpp
+++ recommendation engine/recommendation.cpp
@@ -7,32 +7,44 @@
 
 using namespace std;
 
-RecommendationEngine::RecommendationEngine() {
+// constructor if user didn't choose set preference
+RecommendationEngine::RecommendationEngine() 
+: age(-1), genre("none"), director("none") {}
 
-}
+// constructor if user chooses age as their preference
+RecommendationEngine::RecommendationEngine(int userAge) 
+: age(userAge), genre("none"), director("none") {}
 
-void RecommendationEngine::setAge() const {
+// constructor if user chooses genre as their preference
+RecommendationEngine::RecommendationEngine(string userGenre, int userAge) 
+: age(userAge), genre(userGenre), director("none") {}
 
+// constructor if user chooses director as their preference
+RecommendationEngine::RecommendationEngine(string userDirector) 
+: age(-1), genre("none"), director(userDirector){}
+
+void RecommendationEngine::setAge(const int age) const {
+    this->age = age;
 }
 
 void RecommendationEngine::setGenre(const string genre) const{
-     
+    this->genre = genre;
 }
 
 void RecommendationEngine::setDirector(const string director) const{
-
+    this->director = director;
 }
 
 int RecommendationEngine::getAge() const {
-
+    return age;
 }
 
 string RecommendationEngine::getGenre() const {
-
+    return genre;
 }
 
 string RecommendationEngine::getDirector() const {
-
+    return director;
 }
 
 vector<TVShow> RecommendationEngine::compareData(const vector<TVShow>) const {
--- src/display.cpp
+++ src/display.cpp
@@ -3,7 +3,62 @@
 
 
 void Display::Menu() {
- 
+    // if user chooses enter preferences
+    int input;
+    cout << "Welcome to Ctrl+ALt+Binge!" << endl;
+    cout << "1. Enter Preferences" << endl;
+    cout << "2. Random Recommendation" << endl;
+    cout << "3. Developers' Favorites" << endl;
+    cout << "4. Exit"
+    cout << "Please select an option (1-4): " << endl;
+
+    if (input == 1) {
+        
+        int choice;
+        
+        while (true) {
+            cout << "Choose your Recommendation Based off of: " << endl;
+            cout << "1. Age" << endl;
+            cout << "2. Favorite Genre" << endl;
+            cout << "3. Favorite Director" << endl;
+            cout << "Please select an option (1-3): " << endl;
+            cin >> choice;
+
+            if (choice == 1) {
+                int userAge;
+                cout << "Enter Your Age: (a number please!)" << endl;
+                cin >> userAge;
+                while (true) {
+                    if (cin.fail()) {
+                        cin.clear();
+                        cin.ignore();
+                        cout << "Ok maybe there was some disconnect.. a number (i.e 7 NOT seven or some other words/characters)";
+                    } else {
+                        break;
+                    }
+                }
+                RecommendationEngine ageRecommend(userAge);
+                break;
+            } else if (choice == 2) {
+                string userGenre;
+                cout << "Enter Your Favorite Genre: " << endl;
+                cin >> userGenre;
+                RecommendationEngine genreRecommend(userGenre, -1);
+                break;
+                
+            } else if (choice == 3) {
+                string userDirector;
+                cout << "Enter Your Favorite Director: " << endl;
+                cin >> userDirector;
+                RecommendationEngine directorRecommend(userDirector);
+                break;
+
+            } else {
+                cout << "Now you know that wasn't an option, input again." << endl;
+            }
+    }
+        displayRecommendationsCust();
+    }
 }
 
 void Display::displayRecommendationsDev() {
